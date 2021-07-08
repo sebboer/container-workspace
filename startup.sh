2b044a08af449e11a21c0dffabcd469b57888963
Sebastian Boerakker <sebboer@protonmail.com>
2021-07-08 17:30:22 +0200

    add podman plugins to install script

--- startup.sh
+++ startup.sh
@@ -41,7 +41,7 @@ curl -fLo "$HOME/.zshrc" --create-dirs https://raw.githubusercontent.com/sebboer
 echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/testing/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:testing.list && \
 curl -L "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/testing/xUbuntu_${VERSION_ID}/Release.key" | sudo apt-key add - && \
 sudo apt-get update -qq && \
-sudo apt-get -qq -y install podman
+sudo apt-get -qq -y install podman containernetworking-plugins
 
 # install docker
 sudo apt-get -y install \
